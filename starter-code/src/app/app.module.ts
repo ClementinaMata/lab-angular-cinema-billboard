b6831bce4dbd37975a93eeae0c0e90e5cd7bef08
Clementina <mataclemy@gmail.com>
2017-11-29 19:24:41 +0100

    iteration2

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { CinemaService } from './services/cinema.service';
-
+import { RouterModule } from '@angular/router';
+import { routes} from './routes';
 
 import { AppComponent } from './app.component';
+import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
+import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponentComponent,
+    MyMovieComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [CinemaService],
   bootstrap: [AppComponent]
